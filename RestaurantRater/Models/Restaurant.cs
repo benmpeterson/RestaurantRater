4812de5aadd88be9da5e59e2bea85f47f49c4599
Ben Peterson <ben.micah.peterson@gmail.com>
2017-04-07 09:12:53 -0400

    Initital Commit

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    //creating a sublcass of the DbContext
+    //uses Set as a collection type
+    public class RestaurantDBContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
