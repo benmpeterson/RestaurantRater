fced7e22557a5f960685f9b6d17b6f69b7ae356b
Ben Peterson <ben.micah.peterson@gmail.com>
2017-04-07 12:00:35 -0400

    Added First Database Entry

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,46 @@
+﻿using RestaurantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        //GET: Restaurant
+        private RestaurantDBContext db = new RestaurantDBContext();
+        // GET: Restaurants
+        public ActionResult Index()
+        {
+            return View(db.Restaurants.ToList());
+        }
+
+        //GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include ="RestaurantID, Name")] Restaurant resturant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Restaurants.Add(resturant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(resturant);
+        }
+
+
+
+
+
+
+    }
+}
